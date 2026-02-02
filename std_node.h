bd7ff9f4719aa20faaec40997562cc93785be636
Numberr1st <88944342+Numberr1st@users.noreply.github.com>
2026-02-02 10:36:39 +0000

    Week4

---
+++ std_node.h
@@ -0,0 +1,13 @@
+
+#ifndef node_h
+#define node_h
+
+struct student_node {
+    long student_id;
+    char name[100];
+    struct student_node *next;
+};
+
+typedef struct student_node* StudentPtr;
+
+#endif /* node_h */
